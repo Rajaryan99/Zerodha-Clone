9cf6549d315d96b82004e2418af52fcbc7bbe792
Rajaryan singh <raj.aryan2518@gmail.com>
2025-11-13 00:20:30 +0530

    Database connected

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,26 @@
-import express from 'express'
+const express = require('express');
+const { mongo, default: mongoose } = require('mongoose');
+require('dotenv').config();
+
 
 const app = express()
 
-const port = 8080;
+const PORT = process.env.PORT || 8080;
+const uri = process.env.MONGO_URL
 
 app.get('/', (req, res) => {
     res.send('Helloworld')
 })
 
-app.listen(port, () => {
-    console.log(`Server is running on http://localhost:${port}/`)
+mongoose.connect(uri)
+.then(() => {
+    console.log('DB Connected Successfully');
+    app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}/`);
+    
 })
+})
+.catch((err) => {
+    console.log('MongoDB connection failed', err.message)
+})
+
